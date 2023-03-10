336d6e6d6b10af0b972e87076f60ef8434c22c12
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-10 19:25:12 -0300

    finalizando exercicio da minha musica preferida

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Beggin: Måneskin");
+            Console.ResetColor();
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("Put your loving hand out, baby");
+            Console.WriteLine("'Cause I'm beggin'");
+            Console.WriteLine();
+
+ 
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("So, put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, darling");
+            Console.ResetColor();
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("Riding high, when I was king");
+            Console.WriteLine("I played it hard and fast, 'cause I had everything");
+            Console.WriteLine("I walked away, but you warned me then");
+            Console.WriteLine("But easy come, and easy go, and it would end");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("So, any time I bleed, you let me go");
+            Console.WriteLine("Yeah, any time I feed you, get me? No?");
+            Console.WriteLine("Any time I seek, you let me know");
+            Console.WriteLine("But I plan and see, just let me go");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("'Cause I'm beggin'");
+            Console.WriteLine("I'm on my knees when I'm beggin'");
+            Console.WriteLine("'Cause I don't want to lose you");
+            Console.WriteLine("Hey yeah, ratatata");
+            Console.WriteLine();
+
+ 
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("'Cause I'm beggin', beggin' you");
+            Console.WriteLine("And put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("And put your loving hand out, darling");
+            Console.ResetColor();
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("I need you to understand");
+            Console.WriteLine("Tried so hard to be your man");
+            Console.WriteLine("The kind of man you want in the end");
+            Console.WriteLine("Only then can I begin to live again");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("An empty shell, I used to be");
+            Console.WriteLine("The shadow of my life was hanging over me");
+            Console.WriteLine("A broken man that I don't know");
+            Console.WriteLine("Won't even stand the devil's chance to win my soul");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("What we doing? What we chasing?");
+            Console.WriteLine("Why the bottom? Why the basement?");
+            Console.WriteLine("Why we got good shit, don't embrace it?");
+            Console.WriteLine("Why the feel for the need to replace me?");
+            Console.WriteLine("You're the wrong way track from the good");
+            Console.WriteLine("I want to paint a picture telling where we could be at");
+            Console.WriteLine("Like a heart in the best way should");
+            Console.WriteLine("You can give it away, you had, and you took the pay");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("But I keep walking on, keep opening doors");
+            Console.WriteLine("Keep hoping for, that the door is yours");
+            Console.WriteLine("Keep oh-so home");
+            Console.WriteLine("'Cause I don't wanna live in a broken home");
+            Console.WriteLine("Girl, I'm beggin'");
+            Console.WriteLine();
+
+ 
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Mmm, ye-e-e-ah");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, darling");
+            Console.ResetColor();
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("I'm fighting hard to hold my own");
+            Console.WriteLine("Just can't make it all alone");
+            Console.WriteLine("I'm holding on, I can't fall back");
+            Console.WriteLine("I'm just a calm 'bout to fade to black");
+            Console.WriteLine();
+
+ 
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("Put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, darling");
+            Console.WriteLine();
+        
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, darling");
+            Console.WriteLine();
+
+ 
+
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out, baby");
+            Console.WriteLine("I'm beggin', beggin' you");
+            Console.WriteLine("So, put your loving hand out");
+            Console.ResetColor();
